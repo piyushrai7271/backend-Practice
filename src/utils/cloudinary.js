512064b3868690e94ea50f1aa18d354a0117dc3c
piyushrai7271 <piyushrai7271@gmail.com>
2024-03-22 10:15:12 +0530

    config multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,25 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_Api_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    let response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    console.log("file is uploaded on cloudnery :", response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // it will remove the tempery file which as the uploaded opretion got failed
+    return null;
+  }
+};
+
+
+export {uploadOnCloudinary};
